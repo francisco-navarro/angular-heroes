2be5d4ce341df9cb8b87fcd2e5a21a0e29eea8e8
Paco Navarro <packarbell@gmail.com>
2016-11-25 15:24:21 +0100

    ADD directiva hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -20,12 +20,7 @@ const HEROES: Hero[] = [
   template: `
   <h1>{{title}}</h1>
   <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   </div>
   <ul class="heroes">
     <li *ngFor="let hero of heroes" 
